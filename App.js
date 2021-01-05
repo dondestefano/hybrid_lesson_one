a5a3a094dba4671398e375fc98f1c6e1ec3476e6
dondestefano <dondestefano@users.noreply.github.com>
2021-01-05 20:40:16 +0100

    Adds textinput section.

--- App.js
+++ App.js
@@ -1,16 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.topContainer}/>
+
       <View style={styles.headerContainer}>
         <Text style={styles.headerText}>Hello World!</Text>
         <Text style={styles.titleText}>First React Native App</Text>
         <Text style={styles.blueColor}>This is a paragraph explaining what this app is all about. As you can see there are already many components including text, images and buttons.</Text>
+      </View>
 
+      <View style={styles.container, {width: "100%", padding: 8}}>
+        <Text style={{...styles.titleText, ...styles.bold, width: 250}}>What would you like to get out of this course?</Text>
+        <TextInput style={{ padding: 8, height: 100, backgroundColor: "#F2F2F2", textAlignVertical: 'top'}} placeholder="Enter some text" multiline={true} />
       </View>
       
     </View>
@@ -42,7 +47,16 @@ const styles = StyleSheet.create({
   titleText: {
     fontSize: 18,
   },
+  textInput: {
+    padding: 8, 
+    height: 200, 
+    backgroundColor: "#F2F2F2",
+    
+  },
   blueColor: {
     color: "#24305E"
+  },
+  bold: {
+    fontWeight: "bold"
   }
 });
